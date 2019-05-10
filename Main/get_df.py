786e7517281d2269d1c0a08acc301c2dbd3ea63b
ZayyanShah <41247476+ZayyanShah@users.noreply.github.com>
2019-05-10 01:15:22 -0400

    Update get_df.py

--- Main/get_df.py
+++ Main/get_df.py
@@ -21,7 +21,7 @@ def getcompanytable (companySymbol, marketSymbol, startDate, endDate):
   cursor.execute(sqll)
   result = cursor.fetchall()
   p = pd.DataFrame(data = result)
-  p.columns = ["Date", "Open", "High", "Low", "Close", "Net Chg", "% Chg", "Volume"]
+  p.columns = ["Date", "Open", "High", "Low", "Close", "Net_Chg", "%_Chg", "Volume"]
 
   return p
 
