c64deaf61d43d6c565ae6d6dbef885acde703636
Malcolm Yeh <40870113+malcolmyeh@users.noreply.github.com>
2019-05-10 02:10:40 -0400

    Update tableDriver.py

--- Main/tableDriver.py
+++ Main/tableDriver.py
@@ -12,7 +12,7 @@ def tabledriver ():
             df = getcompanytable(company, market, start, end)
             print(df)
         elif (command == "help"):
-            print("(table command list)")
+            print("Multiple inputs must be space separated.\nCommand         Description\n-------         -----------\nall             display data for all dates\nrange           display data for given range of dates\nback            return to main menu")
         else:
             print("Invalid input. Enter 'help' for more list of commands.")
         command = input("Enter table command: ")
