e6b68a3cc11ff8237ad52528313f83df816ba3d9
Malcolm Yeh <40870113+malcolmyeh@users.noreply.github.com>
2019-05-10 01:20:20 -0400

    Update graphDriver.py

--- Main/graphDriver.py
+++ Main/graphDriver.py
@@ -1,12 +1,22 @@
+from graph import *
+
 def graphdriver ():
     command = input("Enter graph command: ")
     while (command != "back"):
-        if (command == "line"):
-            print("line graph")
-        elif (command == "candlestick"):
-            print("candlestick graph")
-        elif (command == "help"):
-            print("(graph command list)")
+        if (command == "price"):
+            company, market = input("Enter Company and Market Name: ").split()
+            plot_price(company, market)
+        elif (command == "perchg"):
+            company, market = input("Enter Company and Market Name: ").split()
+            plot_perchg(company, market)
+        elif (command == "netchg"):
+            company, market = input("Enter Company and Market Name: ").split()
+            plot_netchg(company, market)
+         elif (command == "volume"):
+            company, market = input("Enter Company and Market Name: ").split()
+            plot_volume(company, market)
+         elif (command == "volume"):
+            print("(help menu)")
         else:
             print("Invalid input. Enter 'help' for more list of commands.")
         command = input("Enter graph command: ")
